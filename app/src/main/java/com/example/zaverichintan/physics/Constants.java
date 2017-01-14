294e86e95ec23bb641d165325c6f25473bff3079
zaverichintan <zaveri_chintan@rediff.com>
2017-01-15 00:21:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/zaverichintan/physics/Constants.java
@@ -0,0 +1,11 @@
+package com.example.zaverichintan.physics;
+
+/**
+ * Created by zaverichintan on 12/1/17.
+ */
+public class Constants {
+
+    public static final String UPLOAD_URL = "http://olbx.in/android_upload/upload.php";
+    public static final String IMAGES_URL = "http://olbx.in/android_upload/getImages.php";
+
+}
